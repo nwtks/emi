54fbf605c2b3f8e25ab0e0db9229ef216bec4ecc
nwtks <44502906+nwtks@users.noreply.github.com>
2018-12-24 09:40:22 +0900

    add semicolon.

--- dist/temi.js
+++ dist/temi.js
@@ -4,30 +4,24 @@ var createEmitter = function () {
   var listeners = Object.create(null);
   var emitter = {
     on: function (e, f) {
-      if (!listeners[e]) {
-        listeners[e] = [];
-      }
+      !listeners[e] && (listeners[e] = []);
       listeners[e].push(f);
-      return emitter
+      return emitter;
     },
     off: function (e, f) {
-      if (listeners[e]) {
-        if (f == null) {
-          listeners[e] = [];
-        } else {
-          listeners[e] = listeners[e].filter(function (a) { return a !== f; });
-        }
-      }
-      return emitter
+      listeners[e] &&
+        (listeners[e] = f == null ? [] : listeners[e].filter(function (a) { return a !== f; }));
+      return emitter;
     },
     emit: function (e, a) {
-      if (listeners[e]) {
-        listeners[e].forEach(function (f) { return f(a); });
-      }
-      return emitter
+      listeners[e] &&
+        listeners[e].forEach(function (f) {
+          f(a);
+        });
+      return emitter;
     }
   };
-  return emitter
+  return emitter;
 };
 
 module.exports = createEmitter;
--- src/temi.js
+++ src/temi.js
@@ -1,31 +1,25 @@
 const createEmitter = () => {
-  const listeners = Object.create(null)
+  const listeners = Object.create(null);
   const emitter = {
     on: (e, f) => {
-      if (!listeners[e]) {
-        listeners[e] = []
-      }
-      listeners[e].push(f)
-      return emitter
+      !listeners[e] && (listeners[e] = []);
+      listeners[e].push(f);
+      return emitter;
     },
     off: (e, f) => {
-      if (listeners[e]) {
-        if (f == null) {
-          listeners[e] = []
-        } else {
-          listeners[e] = listeners[e].filter(a => a !== f)
-        }
-      }
-      return emitter
+      listeners[e] &&
+        (listeners[e] = f == null ? [] : listeners[e].filter((a) => a !== f));
+      return emitter;
     },
     emit: (e, a) => {
-      if (listeners[e]) {
-        listeners[e].forEach(f => f(a))
-      }
-      return emitter
+      listeners[e] &&
+        listeners[e].forEach((f) => {
+          f(a);
+        });
+      return emitter;
     }
-  }
-  return emitter
-}
+  };
+  return emitter;
+};
 
-export default createEmitter
+export default createEmitter;
